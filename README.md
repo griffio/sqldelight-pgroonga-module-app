d285d34c384c9ccf4d0c4525077b07ef1aa1e210
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-07-13 19:05:37 +0100

    Project build files

---
+++ README.md
@@ -0,0 +1,25 @@
+# SqlDelight 2.1.x Postgresql module support prototype 
+
+https://github.com/cashapp/sqldelight
+
+**Experimental**
+
+Pgroonga https://pgroonga.github.io/
+
+Use with SqlDelight `2.1.0`
+
+TODO
+
+* Needs to support `pgroonga_score` with system columns `tableoid, ctid` 
+
+---
+
+```shell
+docker run \
+  --name pgroonga-demo \
+  -e POSTGRES_PASSWORD=PGroonga \
+  -e POSTGRES_DB=PGroonga \
+  -e POSTGRES_USER=PGroonga \
+  -p 5432:5432 \
+  -d groonga/pgroonga:4.0.1-alpine-16-slim
+```
---
+++ settings.gradle.kts
@@ -0,0 +1,38 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "sqldelight-pgroonga-app"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            val vSqlDelight = "2.1.0"
+            val vIntellij = "231.9392.1"
+            version("intellij", vIntellij)
+            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("2.1.0")
+            plugin("sqldelight", "app.cash.sqldelight").version(vSqlDelight)
+            plugin("flyway", "org.flywaydb.flyway").version("10.1.0")
+            library("sqldelight-dialect-api", "app.cash.sqldelight:dialect-api:$vSqlDelight")
+            library("sqldelight-jdbc-driver", "app.cash.sqldelight:jdbc-driver:$vSqlDelight")
+            library("sqldelight-postgresql-dialect", "app.cash.sqldelight:postgresql-dialect:$vSqlDelight")
+            library("sqldelight-compiler-env", "app.cash.sqldelight:compiler-env:$vSqlDelight")
+            library("postgresql-jdbc-driver", "org.postgresql:postgresql:42.5.4")
+            library("flyway-database-postgresql", "org.flywaydb:flyway-database-postgresql:10.1.0")
+            library("google-truth", "com.google.truth:truth:1.4.2")
+            library("intellij-ide", "com.jetbrains.intellij.platform:ide:$vIntellij")
+            library("pgvector", "com.pgvector:pgvector:0.1.6")
+            plugin("intellij", "org.jetbrains.intellij.platform").version("2.1.0")
+            plugin("grammarKitComposer", "com.alecstrong.grammar.kit.composer").version("0.1.12")
+        }
+    }
+}
+
+include("pgroonga-module")
