88067c95de67a829a71e70d7dddd2986cafc10d1
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-07-13 19:06:24 +0100

    Application

---
+++ src/main/kotlin/griffio/Main.kt
@@ -0,0 +1,28 @@
+package griffio
+
+import app.cash.sqldelight.driver.jdbc.asJdbcDriver
+import griffio.migrations.Memos
+import griffio.migrations.SampleTexts
+import griffio.queries.Sample
+import org.postgresql.ds.PGSimpleDataSource
+import kotlin.random.Random
+
+private fun getSqlDriver() = PGSimpleDataSource().apply {
+    setURL("jdbc:postgresql://localhost:5432/PGroonga")
+    applicationName = "App Main"
+    user = "PGroonga"
+    password = "PGroonga"
+}.asJdbcDriver()
+
+fun main() {
+    val driver = getSqlDriver()
+    val sample = Sample(driver)
+    sample.pgroongaQueries.insertMemo(Memos(Random.nextInt(), "PostgreSQL is a relational database management system."))
+    sample.pgroongaQueries.insertMemo(Memos(Random.nextInt(), "Groonga is a fast full text search engine that supports all languages."))
+    sample.pgroongaQueries.insertMemo(Memos(Random.nextInt(), "PGroonga is a PostgreSQL extension that uses Groonga as index."))
+    sample.pgroongaQueries.insertMemo(Memos(Random.nextInt(), "PostgreSQL is a relational database management system."))
+
+    sample.pgroongaQueries.searchMemos("engine").executeAsList().forEach(::println)
+
+
+}
