255c34dc28f5d8a0d3435aae3ffd6e8aa67e767a
griff і⊙ <346896+griffio@users.noreply.github.com>
2026-03-19 19:58:28 +0000

    Update PGroongaModule.kt

--- pgroonga-module/src/main/kotlin/griffio/PGroongaModule.kt
+++ pgroonga-module/src/main/kotlin/griffio/PGroongaModule.kt
@@ -1,11 +1,9 @@
 package griffio
 
-import app.cash.sqldelight.dialect.api.DialectType
 import app.cash.sqldelight.dialect.api.IntermediateType
 import app.cash.sqldelight.dialect.api.PrimitiveType
 import app.cash.sqldelight.dialect.api.SqlDelightModule
 import app.cash.sqldelight.dialect.api.TypeResolver
-import app.cash.sqldelight.dialects.postgresql.PostgreSqlType
 import app.cash.sqldelight.dialects.postgresql.PostgreSqlTypeResolver
 import app.cash.sqldelight.dialects.postgresql.grammar.PostgreSqlParser
 import app.cash.sqldelight.dialects.postgresql.grammar.PostgreSqlParserUtil
@@ -13,39 +11,56 @@ import com.alecstrong.sql.psi.core.psi.SqlExpr
 import com.alecstrong.sql.psi.core.psi.SqlFunctionExpr
 import com.intellij.lang.parser.GeneratedParserUtilBase
 import com.intellij.lang.parser.GeneratedParserUtilBase.Parser
-import com.squareup.kotlinpoet.CodeBlock
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.asTypeName
 import griffio.grammar.PgroongaParser
 import griffio.grammar.PgroongaParserUtil
+import griffio.grammar.PgroongaParserUtil.extension_expr
+import griffio.grammar.PgroongaParserUtil.index_method
+import griffio.grammar.PgroongaParserUtil.storage_parameters
+import griffio.grammar.PgroongaParserUtil.storage_parameter
+
 import griffio.grammar.psi.PGroonaExtensionExpr
 
 class PGroongaModule : SqlDelightModule {
     override fun typeResolver(parentResolver: TypeResolver): TypeResolver = PGroongaTypeResolver(parentResolver)
 
     override fun setup() {
+
+        val previousExtensionExpr = PostgreSqlParserUtil.extension_expr
+        val previousIndexMethod = PostgreSqlParserUtil.index_method
+        val previousStorageParameters = PostgreSqlParserUtil.storage_parameters
+        val previousStorageParameter = PostgreSqlParserUtil.storage_parameter
+
         PgroongaParserUtil.reset()
         PgroongaParserUtil.overridePostgreSqlParser()
         // As the grammar doesn't support inheritance - override type_name manually to try inherited type_name
-        PostgreSqlParserUtil.extension_expr = GeneratedParserUtilBase.Parser { psiBuilder, i ->
-            PgroongaParserUtil.extension_expr?.parse(psiBuilder, i) ?: PgroongaParser.extension_expr_real(psiBuilder, i)
-                    || PostgreSqlParser.extension_expr_real(psiBuilder, i)
+
+        PostgreSqlParserUtil.extension_expr = Parser { psiBuilder, i ->
+            extension_expr?.parse(psiBuilder, i)
+                    ?: PgroongaParser.extension_expr_real(psiBuilder, i)
+                    || previousExtensionExpr?.parse(psiBuilder, i)
+                    ?: PostgreSqlParser.extension_expr_real(psiBuilder, i)
         }
 
         // etc
         PostgreSqlParserUtil.index_method = GeneratedParserUtilBase.Parser { psiBuilder, i ->
-            PgroongaParserUtil.index_method?.parse(psiBuilder, i) ?: PgroongaParser.index_method_real(psiBuilder, i)
-                    || PostgreSqlParser.index_method_real(psiBuilder, i)
+            index_method?.parse(psiBuilder, i)
+                    ?: PgroongaParser.index_method_real(psiBuilder, i)
+                    || previousIndexMethod?.parse(psiBuilder, i)
+                    ?: PostgreSqlParser.index_method_real(psiBuilder, i)
         }
 
-        PostgreSqlParserUtil.storage_parameters = Parser { psiBuilder, i ->
-            PgroongaParserUtil.storage_parameters?.parse(psiBuilder, i) ?: PgroongaParser.storage_parameters_real(psiBuilder, i)
-                    || PostgreSqlParser.storage_parameters_real(psiBuilder, i)
+        PostgreSqlParserUtil.storage_parameters = GeneratedParserUtilBase.Parser { psiBuilder, i ->
+            storage_parameters?.parse(psiBuilder, i)
+                    ?: PgroongaParser.storage_parameters_real(psiBuilder, i)
+                    || previousStorageParameters?.parse(psiBuilder, i)
+                    ?: PostgreSqlParser.storage_parameters_real(psiBuilder, i)
         }
 
-        PostgreSqlParserUtil.storage_parameter = Parser { psiBuilder, i ->
-            PgroongaParserUtil.storage_parameter?.parse(psiBuilder, i) ?: PgroongaParser.storage_parameter_real(psiBuilder, i)
-                    || PostgreSqlParser.storage_parameter_real(psiBuilder, i)
+        PostgreSqlParserUtil.index_method = GeneratedParserUtilBase.Parser { psiBuilder, i ->
+            storage_parameter?.parse(psiBuilder, i)
+                    ?: PgroongaParser.storage_parameter_real(psiBuilder, i)
+                    || previousStorageParameter?.parse(psiBuilder, i)
+                    ?: PostgreSqlParser.storage_parameter_real(psiBuilder, i)
         }
     }
 }
